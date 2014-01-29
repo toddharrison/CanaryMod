9720bc59a0bcffa45c66ad7153658c08d59d082f
darkdiplomat <darkdiplomat@visualillusionsent.net>
2014-01-29 02:35:45 -0600

    ServerListPingHook change

--- src/main/java/net/minecraft/server/network/NetHandlerStatusServer.java
+++ src/main/java/net/minecraft/server/network/NetHandlerStatusServer.java
@@ -1,5 +1,6 @@
 package net.minecraft.server.network;
 
+import com.mojang.authlib.GameProfile;
 import io.netty.util.concurrent.GenericFutureListener;
 import net.canarymod.hook.system.ServerListPingHook;
 import net.minecraft.network.EnumConnectionState;
@@ -13,6 +14,8 @@ import net.minecraft.network.status.server.S01PacketPong;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IChatComponent;
+
+import java.util.Arrays;
 
 public class NetHandlerStatusServer implements INetHandlerStatusServer {
 
@@ -39,17 +42,20 @@ public class NetHandlerStatusServer implements INetHandlerStatusServer {
     public void a(C00PacketServerQuery c00packetserverquery) {
         // CanaryMod: ServerListPingHook
         ServerStatusResponse ssr = this.a.at();
-        ServerListPingHook hook = (ServerListPingHook) new ServerListPingHook(ssr.a().e(), ssr.b().b(), ssr.b().a(), ssr.d()).call();
+        ServerListPingHook hook = (ServerListPingHook) new ServerListPingHook(ssr.a().e(), ssr.b().b(), ssr.b().a(), ssr.d(), Arrays.asList(ssr.b().c())).call();
         if (hook.isCanceled()) {
             // Response Denied!
             return;
         }
         // Recreate the ServerStatusResponse to be sent so that the default isn't destroyed
         ssr = new ServerStatusResponse();
-        ssr.a(new ServerStatusResponse.MinecraftProtocolVersionIdentifier("1.7.2", 4)); //Protocol (do not change this at all!)
-        ssr.a(new ServerStatusResponse.PlayerCountData(hook.getMaxPlayers(), hook.getCurrentPlayers())); //Max/Online Players
+        ssr.a(new ServerStatusResponse.MinecraftProtocolVersionIdentifier("1.7.2", 4)); //Protocol (do not change this at all [except in the case of updating to new Minecraft]!)
+        ServerStatusResponse.PlayerCountData ssrpcd = new ServerStatusResponse.PlayerCountData(hook.getMaxPlayers(), hook.getCurrentPlayers());
+        ssrpcd.a(hook.getProfiles().toArray(new GameProfile[hook.getProfiles().size()]));
+        ssr.a(ssrpcd); //Max/Online Players & GameProfiles
         ssr.a((IChatComponent) (new ChatComponentText(hook.getMotd()))); //MOTD
         ssr.a(hook.getFavicon()); // Server Favicon
+
         this.b.a(new S00PacketServerInfo(ssr), new GenericFutureListener[0]);
         //
     }
