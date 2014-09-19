d4db6729931d483c55e9beeb275a367d2d8881f5
darkdiplomat <darkdiplomat@visualillusionsent.net>
2014-09-19 08:26:11 -0500

    I reject Mojang logic and substitute my own

--- src/main/java/net/minecraft/command/server/CommandTeleport.java
+++ src/main/java/net/minecraft/command/server/CommandTeleport.java
@@ -1,8 +1,8 @@
 package net.minecraft.command.server;
 
-
 import net.canarymod.Canary;
 import net.canarymod.api.world.CanaryWorld;
+import net.canarymod.api.world.UnknownWorldException;
 import net.canarymod.hook.player.TeleportHook;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
@@ -11,7 +11,7 @@ import net.minecraft.command.WrongUsageException;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.WorldServer;
+import net.visualillusionsent.utils.BooleanUtils;
 
 import java.util.List;
 
@@ -36,7 +36,7 @@ public class CommandTeleport extends CommandBase {
         else {
             EntityPlayerMP entityplayermp;
 
-            if (astring.length != 2 && astring.length != 4) {
+            if (astring[0].matches("\\d+")) { // CanaryMod: check for digit instead of some crazy stupid mess
                 entityplayermp = b(icommandsender);
             }
             else {
@@ -46,7 +46,10 @@ public class CommandTeleport extends CommandBase {
                 }
             }
 
+            /* CanaryMod: Change this whole thing out
             if (astring.length != 3 && astring.length != 4) {
+            */
+            if (astring.length < 3) {
                 if (astring.length == 1 || astring.length == 2) {
                     EntityPlayerMP entityplayermp1 = d(icommandsender, astring[astring.length - 1]);
 
@@ -54,12 +57,12 @@ public class CommandTeleport extends CommandBase {
                         throw new PlayerNotFoundException();
                     }
 
-                    /* CanaryMod: Allow interdimensional teleporting
-                    if (entityplayermp1.o != entityplayermp.o) {
+                    // CanaryMod: Allow interdimensional teleporting for those ignoring restrictions
+                    boolean interdimensional = icommandsender instanceof EntityPlayerMP ? ((EntityPlayerMP)icommandsender).getPlayer().canIgnoreRestrictions() : true;
+                    if (entityplayermp1.o != entityplayermp.o && interdimensional) {
                         a(icommandsender, this, "commands.tp.notSameDimension", new Object[0]);
                         return;
                     }
-                    */
 
                     entityplayermp.a((Entity) null);
                     // CanaryMod: Multiworld Fix and Teleportation Cause
@@ -68,20 +71,33 @@ public class CommandTeleport extends CommandBase {
                 }
             }
             else if (entityplayermp.o != null) {
-                int i0 = astring.length > 5 ? astring.length - 5 : astring.length - 3;
+                // CanaryMod: Redo this int i0 mess
+                int i0 = astring[0].matches("\\d+") ? 0 : 1;
                 double d0 = a(icommandsender, entityplayermp.s, astring[i0++]);
                 double d1 = a(icommandsender, entityplayermp.t, astring[i0++], 0, 0);
                 double d2 = a(icommandsender, entityplayermp.u, astring[i0++]);
 
-                // CanaryMod: Add param for world specifications
-                WorldServer worldServer = null;
-                boolean forceLoad;
-                if (astring.length > 5) {
-                    worldServer = (WorldServer) ((CanaryWorld) Canary.getServer().getWorld(astring[i0])).getHandle();
+                // CanaryMod: Add params for world specifications
+                CanaryWorld canaryWorld = null;
+                boolean load = false;
+                if (astring.length > 4) {
+                    if (!Canary.getServer().getWorldManager().worldExists(astring[i0])) {
+                        a(icommandsender, this, "World non-existant or not loaded", new Object[0]);
+                        return;
+                    }
+                    if (astring.length > 5) {
+                        load = BooleanUtils.parseBoolean(astring[i0 + 1]);
+                    }
+                    try {
+                        canaryWorld = (CanaryWorld)Canary.getServer().getWorldManager().getWorld(astring[i0], load);
+                    }
+                    catch (UnknownWorldException uwex) {
+                        // damagefilter and his pointless exception throwing...
+                    }
                 }
 
-                if (worldServer != null && entityplayermp.o != worldServer) {
-                    Canary.getServer().getConfigurationManager().switchDimension(entityplayermp.getPlayer(), worldServer.getCanaryWorld(), false);
+                if (canaryWorld != null && entityplayermp.o != canaryWorld.getHandle()) {
+                    Canary.getServer().getConfigurationManager().switchDimension(entityplayermp.getPlayer(), canaryWorld, false);
                 }
                 //
 
