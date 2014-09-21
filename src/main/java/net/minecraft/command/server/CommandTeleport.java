e7a51180b6f7fae13f36c1fed1f2ce7387278713
darkdiplomat <darkdiplomat@visualillusionsent.net>
2014-09-20 19:43:31 -0500

    fixing an issue with only specifying a tpto target

--- src/main/java/net/minecraft/command/server/CommandTeleport.java
+++ src/main/java/net/minecraft/command/server/CommandTeleport.java
@@ -34,41 +34,43 @@ public class CommandTeleport extends CommandBase {
             throw new WrongUsageException("commands.tp.usage", new Object[0]);
         }
         else {
-            EntityPlayerMP entityplayermp;
+            // CanaryMod: just rewite this stuff
+            EntityPlayerMP entityplayermp = d(icommandsender, astring[0]);
 
-            if (astring[0].matches("\\d+")) { // CanaryMod: check for digit instead of some crazy stupid mess
+            if (astring[0].matches("\\d+") || (astring.length < 3 && astring[0].equals(icommandsender.b_())) || (astring.length == 1)) {
                 entityplayermp = b(icommandsender);
             }
-            else {
-                entityplayermp = d(icommandsender, astring[0]);
-                if (entityplayermp == null) {
-                    throw new PlayerNotFoundException();
-                }
+
+            if (entityplayermp == null) {
+                throw new PlayerNotFoundException();
             }
+            //
 
             /* CanaryMod: Change this whole thing out
             if (astring.length != 3 && astring.length != 4) {
             */
             if (astring.length < 3) {
-                if (astring.length == 1 || astring.length == 2) {
-                    EntityPlayerMP entityplayermp1 = d(icommandsender, astring[astring.length - 1]);
+                EntityPlayerMP entityplayermp1 = d(icommandsender, astring[astring.length - 1]);
 
-                    if (entityplayermp1 == null) {
-                        throw new PlayerNotFoundException();
-                    }
-
-                    // CanaryMod: Allow interdimensional teleporting for those ignoring restrictions
-                    boolean interdimensional = icommandsender instanceof EntityPlayerMP ? ((EntityPlayerMP)icommandsender).getPlayer().canIgnoreRestrictions() : true;
-                    if (entityplayermp1.o != entityplayermp.o && interdimensional) {
-                        a(icommandsender, this, "commands.tp.notSameDimension", new Object[0]);
-                        return;
-                    }
-
-                    entityplayermp.a((Entity) null);
-                    // CanaryMod: Multiworld Fix and Teleportation Cause
-                    entityplayermp.a.a(entityplayermp1.s, entityplayermp1.t, entityplayermp1.u, entityplayermp1.y, entityplayermp1.z, entityplayermp1.getCanaryWorld().getType().getId(), entityplayermp1.getCanaryWorld().getName(), TeleportHook.TeleportCause.COMMAND);
-                    a(icommandsender, this, "commands.tp.success", new Object[]{entityplayermp.b_(), entityplayermp1.b_()});
+                if (entityplayermp1 == null) {
+                    throw new PlayerNotFoundException();
                 }
+                else if (entityplayermp == entityplayermp1) {
+                    Canary.log.debug("FUCKUP!");
+                    return;
+                }
+
+                // CanaryMod: Allow interdimensional teleporting for those ignoring restrictions
+                boolean interdimensional = icommandsender instanceof EntityPlayerMP ? ((EntityPlayerMP) icommandsender).getPlayer().canIgnoreRestrictions() : true;
+                if (entityplayermp1.o != entityplayermp.o && interdimensional) {
+                    a(icommandsender, this, "commands.tp.notSameDimension", new Object[0]);
+                    return;
+                }
+
+                entityplayermp.a((Entity) null);
+                // CanaryMod: Multiworld Fix and Teleportation Cause
+                entityplayermp.a.a(entityplayermp1.s, entityplayermp1.t, entityplayermp1.u, entityplayermp1.y, entityplayermp1.z, entityplayermp1.getCanaryWorld().getType().getId(), entityplayermp1.getCanaryWorld().getName(), TeleportHook.TeleportCause.COMMAND);
+                a(icommandsender, this, "commands.tp.success", new Object[]{entityplayermp.b_(), entityplayermp1.b_()});
             }
             else if (entityplayermp.o != null) {
                 // CanaryMod: Redo this int i0 mess
@@ -105,6 +107,9 @@ public class CommandTeleport extends CommandBase {
                 entityplayermp.a(d0, d1, d2);
 
                 a(icommandsender, this, "commands.tp.success.coordinates", new Object[]{entityplayermp.b_(), Double.valueOf(d0), Double.valueOf(d1), Double.valueOf(d2)});
+            }
+            else {
+                throw new WrongUsageException("commands.tp.usage", new Object[0]);
             }
         }
     }
