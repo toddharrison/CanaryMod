04c76fbc0bb70282ae69a6c08a094649d2c7a392
somners <somers.ans@gmail.com>
2014-10-12 11:12:23 -0400

    implements AIFactory additions.

--- src/main/java/net/canarymod/api/factory/CanaryAIFactory.java
+++ src/main/java/net/canarymod/api/factory/CanaryAIFactory.java
@@ -3,18 +3,19 @@ package net.canarymod.api.factory;
 import com.google.common.base.Predicate;
 import java.lang.reflect.Method;
 import net.canarymod.api.ai.*;
-import net.canarymod.api.entity.living.CanaryEntityLiving;
-import net.canarymod.api.entity.living.EntityLiving;
-import net.canarymod.api.entity.living.LivingBase;
+import net.canarymod.api.entity.living.*;
 import net.canarymod.api.entity.living.animal.CanaryWolf;
+import net.canarymod.api.entity.living.animal.EntityAnimal;
+import net.canarymod.api.entity.living.animal.Tameable;
 import net.canarymod.api.entity.living.animal.Wolf;
-import net.canarymod.api.entity.living.monster.CanaryEntityMob;
-import net.canarymod.api.entity.living.monster.EntityMob;
-import net.canarymod.api.entity.living.monster.RangedAttackMob;
+import net.canarymod.api.entity.living.humanoid.CanaryVillager;
+import net.canarymod.api.entity.living.humanoid.Villager;
+import net.canarymod.api.entity.living.monster.*;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.IRangedAttackMob;
 import net.minecraft.entity.ai.*;
+import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.entity.passive.EntityWolf;
 
 /**
@@ -22,7 +23,13 @@ import net.minecraft.entity.passive.EntityWolf;
  * @author Aaron
  */
 public class CanaryAIFactory implements AIFactory {
+    //return () new EntityAI(((Canary)).getHandle()).getCanaryAIBase();
     
+    /**
+     * Helper method to get the nms class that corellates to our cms class
+     * @param clazz the cms class to test for
+     * @return an nms class
+     */
     private Class getNMSClass(Class clazz) {
         Class canaryClazz;
         Method method = null;
@@ -70,6 +77,51 @@ public class CanaryAIFactory implements AIFactory {
     @Override
     public AIControlledByPlayer newAIControlledByPlayer(EntityLiving entity, float speed) {
         return (AIControlledByPlayer) new EntityAIControlledByPlayer(((CanaryEntityLiving)entity).getHandle(), speed).getCanaryAIBase();
+    }
+
+    @Override
+    public AICreeperSwell newAICreeperSwell(Creeper creeper) {
+        return (AICreeperSwell) new EntityAICreeperSwell(((CanaryCreeper)creeper).getHandle()).getCanaryAIBase();
+    }
+
+    @Override
+    public AIDefendVillage newAIDefendVillage(IronGolem ironGolem) {
+        return (AIDefendVillage) new EntityAIDefendVillage(((CanaryIronGolem)ironGolem).getHandle()).getCanaryAIBase();
+    }
+
+    @Override
+    public AIEatGrass newAIEatGrass(EntityLiving entity) {
+        return (AIEatGrass) new EntityAIEatGrass(((CanaryEntityLiving)entity).getHandle()).getCanaryAIBase();
+    }
+
+    @Override
+    public AIFindEntityNearest newAIFindEntityNearest(EntityLiving entityLiving, Class<? extends net.canarymod.api.entity.Entity> entityClass) {
+        return (AIFindEntityNearest) new EntityAIFindEntityNearest(((CanaryEntityLiving)entityLiving).getHandle(), this.getNMSClass(entityClass)).getCanaryAIBase();
+    }
+
+    @Override
+    public AIFindEntityNearestPlayer newAIFindEntityNearestPlayer(EntityLiving entityLiving) {
+        return (AIFindEntityNearestPlayer) new EntityAIFindEntityNearestPlayer(((CanaryEntityLiving)entityLiving).getHandle()).getCanaryAIBase();
+    }
+
+    @Override
+    public AIFleeSun newAIFleeSun(EntityMob mob, double speed) {
+        return (AIFleeSun) new EntityAIFleeSun((EntityCreature)((CanaryEntityMob)mob).getHandle(), speed).getCanaryAIBase();
+    }
+
+    @Override
+    public AIFollowGolem newAIFollowGolem(Villager villager) {
+        return (AIFollowGolem) new EntityAIFollowGolem(((CanaryVillager)villager).getHandle()).getCanaryAIBase();
+    }
+
+    @Override
+    public AIFollowOwner newAIFollowOwner(Tameable entity, double speed, float minDistance, float maxDistance) {
+        return (AIFollowOwner) new EntityAIFollowOwner((EntityTameable)((CanaryEntityLiving)entity).getHandle(), speed, minDistance, maxDistance).getCanaryAIBase();
+    }
+
+    @Override
+    public AIFollowParent newAIFollowParent(EntityAnimal animal, double speed) {
+        return (AIFollowParent) new EntityAIFollowParent((net.minecraft.entity.passive.EntityAnimal)((CanaryEntityLiving)animal).getHandle(), speed).getCanaryAIBase();
     }
     
     private class WrappedPredicate implements Predicate {
